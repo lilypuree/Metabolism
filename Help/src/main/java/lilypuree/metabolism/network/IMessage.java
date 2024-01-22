f66d860b59f491fd3ec0ef1742f709e837e91e8a
lilypuree <000abczyx@snu.ac.kr>
2024-01-23 00:32:05 +0900

    fixing

--- Help/src/main/java/lilypuree/metabolism/network/IMessage.java
+++
@@ -1,8 +0,0 @@
-package lilypuree.metabolism.network;
-
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.ResourceLocation;
-
-public interface IMessage {
-    void toBytes(FriendlyByteBuf buf);
-}
